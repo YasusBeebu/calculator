2aeefa90c895fa1d3e534f97afa0cc9390936c9b
Jim Void <8578834+YasusBeebu@users.noreply.github.com>
2022-01-30 02:07:06 +0400

    add keyboard press animations

--- js/main.js
+++ js/main.js
@@ -95,9 +95,11 @@ const calculator = {
 }
 
 // Event Bindings
+const buttons = Array.from(document.querySelectorAll('.button'))
+
 const elementsArr = [
-    '.deleteButton', 
-    '.clearButton', 
+    '.deleteButton',
+    '.clearButton',
     '.division',
     '.equals',
     '.multi',
@@ -121,102 +123,121 @@ function getElement(el_class) {
 }
 
 function clickAnimation() {
-    this.classList.add('test');
-    setTimeout(() => (this.classList.remove('test')), 150);
+    this.classList.add('pressTheButton');
+    setTimeout(() => (this.classList.remove('pressTheButton')), 150);
 }
 
 function bindOnClick(el_class) {
     getElement(el_class).addEventListener('click', clickAnimation)
 }
 
-elementsArr.forEach(el => (bindOnClick(el, clickAnimation)))
+elementsArr.forEach(el => (bindOnClick(el, clickAnimation))) //add press animation
 
-//getElement('.n0').addEventListener('click', () => (calculator.setValue(0)));
-//getElement('.n1').addEventListener('click', () => (calculator.setValue(1)))
-//getElement('.n2').addEventListener('click', () => (calculator.setValue(2)))
-//getElement('.n3').addEventListener('click', () => (calculator.setValue(3)))
-//getElement('.n4').addEventListener('click', () => (calculator.setValue(4)))
-//getElement('.n5').addEventListener('click', () => (calculator.setValue(5)))
-//getElement('.n6').addEventListener('click', () => (calculator.setValue(6)))
-//getElement('.n7').addEventListener('click', () => (calculator.setValue(7)))
-//getElement('.n8').addEventListener('click', () => (calculator.setValue(8)))
-//getElement('.n9').addEventListener('click', () => (calculator.setValue(9)))
-//getElement('.dot').addEventListener('click', () => (calculator.float()))
-//getElement('.sum').addEventListener('click', () => (calculator.math('+')))
-//getElement('.min').addEventListener('click', () => (calculator.math('-')))
-//getElement('.multi').addEventListener('click', () => (calculator.math('*')))
-//getElement('.division').addEventListener('click', () => (calculator.math('/')))
-//getElement('.equals').addEventListener('click', () => (calculator.math('=')))
-//getElement('.clearButton').addEventListener('click', () => (calculator.clear()))
-//getElement('.deleteButton').addEventListener('click', () => (calculator.delete()))
+getElement('.n0').addEventListener('click', () => (calculator.setValue(0)));
+getElement('.n1').addEventListener('click', () => (calculator.setValue(1)))
+getElement('.n2').addEventListener('click', () => (calculator.setValue(2)))
+getElement('.n3').addEventListener('click', () => (calculator.setValue(3)))
+getElement('.n4').addEventListener('click', () => (calculator.setValue(4)))
+getElement('.n5').addEventListener('click', () => (calculator.setValue(5)))
+getElement('.n6').addEventListener('click', () => (calculator.setValue(6)))
+getElement('.n7').addEventListener('click', () => (calculator.setValue(7)))
+getElement('.n8').addEventListener('click', () => (calculator.setValue(8)))
+getElement('.n9').addEventListener('click', () => (calculator.setValue(9)))
+getElement('.dot').addEventListener('click', () => (calculator.float()))
+getElement('.sum').addEventListener('click', () => (calculator.math('+')))
+getElement('.min').addEventListener('click', () => (calculator.math('-')))
+getElement('.multi').addEventListener('click', () => (calculator.math('*')))
+getElement('.division').addEventListener('click', () => (calculator.math('/')))
+getElement('.equals').addEventListener('click', () => (calculator.math('=')))
+getElement('.clearButton').addEventListener('click', () => (calculator.clear()))
+getElement('.deleteButton').addEventListener('click', () => (calculator.delete()))
 
 //Key Bindings
-
+function keyAnimation(element_class) {// keyboard press animations
+    getElement(element_class).classList.add('pressTheButton');
+    setTimeout(() => (getElement(element_class).classList.remove('pressTheButton')), 150);
+}
 window.addEventListener('keydown', function (event) {
     switch (event.code) {
         case 'Numpad1':
         case 'Digit1':
-            bindOnClick('.n1', clickAnimation)
+            keyAnimation('.n1')
             calculator.setValue(1)
             break;
         case 'Numpad2':
         case 'Digit2':
+            keyAnimation('.n2')
             calculator.setValue(2)
             break;
         case 'Numpad3':
         case 'Digit3':
+            keyAnimation('.n3')
             calculator.setValue(3)
             break;
         case 'Numpad4':
         case 'Digit4':
+            keyAnimation('.n4')
             calculator.setValue(4)
             break;
         case 'Numpad5':
         case 'Digit5':
+            keyAnimation('.n5')
             calculator.setValue(5)
             break;
         case 'Numpad6':
         case 'Digit6':
+            keyAnimation('.n6')
             calculator.setValue(6)
             break;
         case 'Numpad7':
         case 'Digit7':
+            keyAnimation('.n7')
             calculator.setValue(7)
             break;
         case 'Numpad8':
         case 'Digit8':
+            keyAnimation('.n8')
             calculator.setValue(8)
             break;
         case 'Numpad9':
         case 'Digit9':
+            keyAnimation('.n9')
             calculator.setValue(9)
             break;
         case 'Numpad0':
         case 'Digit0':
+            keyAnimation('.n0')
             calculator.setValue(0)
             break;
         case 'NumpadAdd':
         case 'Equal':
+            keyAnimation('.sum')
             calculator.math('+')
             break;
         case 'NumpadSubtract':
         case 'Minus':
+            keyAnimation('.min')
             calculator.math('-')
             break;
         case 'NumpadDivide':
+            keyAnimation('.division')
             calculator.math('/')
             break;
         case 'NumpadMultiply':
+            keyAnimation('.multi')
             calculator.math('*')
             break;
         case 'NumpadDecimal':
+            keyAnimation('.dot')
             calculator.float()
             break;
         case 'NumpadEnter':
         case 'Enter':
+            keyAnimation('.equal')
             calculator.math('=')
             break;
         case 'Backspace':
+            keyAnimation('.deleteButton')
             calculator.delete()
             break;
     }
